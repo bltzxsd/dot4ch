9881f5db5a6d283686ae403ac729ad400e84f24c
bltzxsd <blissardartinq@gmail.com>
2021-05-25 16:40:57 +0530

    added examples

---
+++ examples/catalog/src/main.rs
@@ -0,0 +1,29 @@
+use simple_logger::SimpleLogger;
+use dot4ch::{Update, threadlist::Catalog, Client};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // setting up logging.
+    SimpleLogger::new().init()?;
+
+    // We first make a client: 
+    let client = Client::new();
+
+    // Then we make a catalog for our desired board.
+    // Here I'll pick /g/
+    let g = Catalog::new(&client, "g").await?;
+    
+
+    // You can directly print the catalog.
+    // For more information on what's printed,
+    // I suggest you check out the fmt::Display impl on threadlist::Catalog
+    println!("{}", g);
+
+    // Okay, now that some time has passed we want to update the catalog.
+    let g = g.update(&client).await?;
+    
+    // and print that
+    println!("{}", g);
+
+    Ok(())
+}    
